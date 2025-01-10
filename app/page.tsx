7f109cf0883652aa553cf0662579d9c1beb07000
Visura13 <visurasurajitha@gmail.com>
2025-01-10 21:26:54 +0530

    optimize

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import AboutUs from "@/components/about-us";
-import Albums from "@/components/albums";
-import ContactUs from "@/components/contact-us";
+// import Albums from "@/components/albums";
+// import ContactUs from "@/components/contact-us";
+// import Testimonials from "@/components/testimonials";
 import Hero from "@/components/hero";
-import Hero2 from "@/components/hero2";
 import Services from "@/components/services-section";
-import Testimonials from "@/components/testimonials";
+
+import dynamic from 'next/dynamic';
+const Albums = dynamic(() => import('@/components/albums'), { ssr: false });
+const Testimonials = dynamic(() => import('@/components/testimonials'), { ssr: false });
+const ContactUs = dynamic(() => import('@/components/contact-us'), { ssr: false });
 
 export default function Home() {
   return (
     <main className="bg-[#f7f5ed] font-serif">
       <Hero />
-      {/* <Hero2 /> */}
       <AboutUs />
       <Services />
       <Albums />
